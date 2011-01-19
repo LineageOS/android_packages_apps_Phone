9443bd0f3d1265f2f1568f30774dcf0135f4cbbe
Peter Fares <mrpeter05@gmail.com>
2011-01-19 02:00:38 -0600

    Restore unknown phone number after toggling airplane mode
    Change-Id: I1a92422c3cddb93d356f2a28041df8550bee8f7a

--- src/com/android/phone/MyPhoneNumber.java
+++ src/com/android/phone/MyPhoneNumber.java
@@ -22,8 +22,13 @@ public class MyPhoneNumber extends BroadcastReceiver {
 
         String phoneNum = mTelephonyMgr.getLine1Number();
         String savedNum = prefs.getString(MSISDNEditPreference.PHONE_NUMBER, null);
-
-        if (phoneNum == null) {
+	boolean airplaneModeOn = intent.getBooleanExtra("state", false);
+        
+	if (airplaneModeOn) {
+            if (DBG)
+                Log.d(LOG_TAG, "Airplane Mode On.  No modification to phone number.");
+        }
+        else if (phoneNum == null) {
             if (DBG)
                 Log.d(LOG_TAG, "Trying to read the phone number from file");
 
