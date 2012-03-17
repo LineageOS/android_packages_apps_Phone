502a3346025afcddc505b35af89e02dc306db3ee
nullghost <justin.b.wood@gmail.com>
2012-03-17 16:06:34 -0400

    Phone: Sprint-only quirk: Phantom message wait indicator workaround (DO NOT COMMIT, NEEDSWORK)
    This option disables the so called "Phantom Voicemail" message wait
    indicator notification that is impossible to dismiss on certain accounts
    on Sprint.  In rare instances, feature phone voicemail gets stuck in
    a Sprint account where smart phones are unable to delete them.  Sprint
    will not help the subscriber fix this, as their own Android phones
    lack message wait indicators, as the standard is Visual Voicemail.
    
    When you enable enable_mwinotif_option in overlay/packages/apps/Phone/res/values/config.xml
    this option appears in the Phone app:
    
    Settings > Voicemail Notifications
    
    Default is disabled, as most Sprint phones have no reason to see
    message wait indicators.  It can however be enabled for Sprint phones
    used on overseas carriers where standard message wait indicators are
    used instead of Visual Voicemail.
    
    Patch Set 7: Rebase
                 s/vmnotif/mwinotif/
                 Write better description explaining s/voicemail/mwi/
                 TODO: Rename menu option, implement nebkat's suggestions from Patch Set 6
    
    Change-Id: Id05460c16545f841593dfb3c2c1d1d04af079b26

--- src/com/android/phone/CallNotifier.java
+++ src/com/android/phone/CallNotifier.java
@@ -27,6 +27,7 @@ import com.android.internal.telephony.Connection;
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneBase;
 import com.android.internal.telephony.CallManager;
+import com.android.phone.CallFeaturesSetting;
 
 import android.app.ActivityManagerNative;
 import android.content.Context;
@@ -1334,6 +1335,15 @@ public class CallNotifier extends Handler
             // *should* be blocked at the telephony layer on non-voice-capable
             // capable devices.)
             Log.w(LOG_TAG, "Got onMwiChanged() on non-voice-capable device! Ignoring...");
+            return;
+        }
+
+        boolean notifprop = mApplication.getResources().getBoolean(R.bool.enable_mwinotif_option);
+        boolean notifoption = Settings.System.getInt(mApplication.getPhone().getContext().getContentResolver(), CallFeaturesSetting.ENABLE_VOICEMAIL_NOTIFICATION, 0) == 1;
+        if (notifprop && !notifoption) {
+            // enable_mwinotif_option is true, and enable_voicemail_notification is unchecked or unset (false)
+            // ignore the mwi event, but log if we're debugging.
+            if (VDBG) log("onMwiChanged(): mwinotif is disabled. Ignoring...");
             return;
         }
 
