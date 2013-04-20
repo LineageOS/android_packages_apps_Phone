46e6d0640b0f93cf887292b34f0350e47b81faac
YuanQY <yuanqingyun@gmail.com>
2013-04-20 21:05:37 +0800

    Add feature for call record. Snapshots: http://i1342.photobucket.com/albums/o774/engle_mars/Screenshot_2013-04-20-20-58-19_zps32fe8f76.png http://i1342.photobucket.com/albums/o774/engle_mars/Screenshot_2013-04-20-20-58-27_zpsad9e98c8.png http://i1342.photobucket.com/albums/o774/engle_mars/Screenshot_2013-04-20-20-58-07_zps2f269d9d.png http://i1342.photobucket.com/albums/o774/engle_mars/Screenshot_2013-04-20-20-58-37_zps8ba18cc8.png
    Change-Id: Ic093de51858fa8b608b3e57d84b61868ce62f94b

--- src/com/android/phone/InCallScreen.java
+++ src/com/android/phone/InCallScreen.java
@@ -1642,6 +1642,10 @@ public class InCallScreen extends Activity
                 onMuteClick();
                 return true;
 
+            case KeyEvent.KEYCODE_MEDIA_RECORD:
+                onRecordClick();
+                return true;
+
             // Various testing/debugging features, enabled ONLY when VDBG == true.
             case KeyEvent.KEYCODE_SLASH:
                 if (VDBG) {
@@ -2978,6 +2982,13 @@ public class InCallScreen extends Activity
         PhoneUtils.setMute(newMuteState);
     }
 
+    private void onRecordClick() {
+        boolean newRecordState = !PhoneUtils.isRecording();
+        if (DBG)
+            log("onRecordClick(): newRecordState = " + newRecordState);
+        PhoneUtils.turnOnRecord(newRecordState);
+    }
+
     /**
      * Toggles whether or not to route in-call audio to the bluetooth
      * headset, or do nothing (but log a warning) if no bluetooth device
@@ -3165,6 +3176,10 @@ public class InCallScreen extends Activity
                 break;
             case R.id.addBlacklistButton:
                 confirmAddBlacklist();
+                break;
+
+            case R.id.recordButton:
+                onRecordClick();
                 break;
 
             default:
